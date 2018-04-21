93ab6a319788fbf29b36f6683bb089b705ad669b
tiwansh <at.slimshady@gmail.com>
2018-04-21 14:18:17 +0530

    Testing signup form

--- routes.py
+++ routes.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
+from models import db
+from forms  import SignupForm
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+db.init_app(app)
+app.secret_key = "development-key"
 
+@app.route("/")
 def index():
 	return render_template("index.html")
 
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
+
+@app.route("/signup")
+def signup():
+	form = SignupForm()
+	return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
 	app.run(debug = True)
